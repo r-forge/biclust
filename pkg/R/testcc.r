5e23d7e721ccf78bc68a9d2e54780de6fca3c902
kingsebi (Sebastian Kaiser) <sebastian.kaiser@stat.uni-muenchen.de>
2007-05-14 13:22:22 +0000

    cc added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biclust@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/testcc.r
@@ -0,0 +1,14 @@
+source('cc.r')
+
+test<-matrix(rbinom(4000,50,0.4),200,200)
+logr<-rep(TRUE,nrow(test))
+logc<-rep(TRUE,ncol(test))
+
+ccscore(test)
+erg<-bigcc(test,delta=1.5,alpha=1)
+
+print(test[erg[[1]],erg[[2]]])
+
+
+erg2<-ccbiclust(test,delta=1.5,alpha=1)
+
