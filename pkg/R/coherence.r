5b6220e10761f80aed2101cf49a5c9f682e9c528
rodrigo (Rodrigo Santamaría) <rodri@usal.es>
2007-06-19 09:40:28 +0000

    First approach to coherence measures for constant and coherent (additive, multiplicative, sign) biclusters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biclust@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/coherence.r
@@ -0,0 +1,250 @@
+# Functions to compute different kinds of coherence measures on biclusters
+# We consider measures to check sign coherent biclusters, 
+# multiplicative and additive biclusters and constant biclusters, all of them
+# by rows and/or columns.
+
+
+
+
+# Equivalent function to diff() but that divides instead of substracting
+# each row or column by the before/above row/column.
+# mat - matrix nxm to compute
+# rowOrColumn - 1 makes divisions by rows, while 2 makes divisions by column.
+#               Default is 1
+# returns - a matrix (n-1)xm or nx(m-1) (depending on rowOrColumns) where row/
+#           column i is the result of dividing row/column i+1 by row/column i
+#           of input matrix.
+# NOTE: divisions by zero are computed as Inf.
+
+#Author: Rodrigo Santamaría
+div=function(mat,rowOrColumn=1)
+  {
+  n=dim(mat)[1]
+  m=dim(mat)[2]
+
+  if(rowOrColumn==1)
+    {
+    ret=matrix(NA,n,m)
+    ret[1,]=rep(1,m)
+    for(i in 2:n)
+       {
+       ret[i,]=mat[i,]/mat[(i-1),]
+       }
+    }
+  else if(rowOrColumn==2)
+    {
+    ret=matrix(NA,n,m)
+    ret[,1]=rep(1,n)
+    for(j in 2:m)
+       {
+       ret[,j]=mat[,j]/mat[,(j-1)]
+       }
+    }
+  ret
+  }
+
+# Adaptation of diff() to equal div() interface
+# mat - matrix nxm to compute
+# rowOrColumn - 1 makes divisions by rows, while 2 makes divisions by column.
+#               Default is 1
+# returns - a matrix (n-1)xm or nx(m-1) (depending on rowOrColumns) where row/
+#           column i is the result of dividing row/column i+1 by row/column i
+#           of input matrix.
+dif=function(mat,rowOrColumn=1)
+  {
+  n=dim(mat)[1]
+  m=dim(mat)[2]
+
+  if(rowOrColumn==1)
+    {
+    ret=matrix(NA,n,m)
+    ret[1,]=rep(0,m)
+    for(i in 2:n)
+       {
+       ret[i,]=mat[i,]-mat[(i-1),]
+       }
+    }
+  else if(rowOrColumn==2)
+    {
+    ret=matrix(NA,n,m)
+    ret[,1]=rep(0,n)
+    for(j in 2:m)
+       {
+       ret[,j]=mat[,j]-mat[,(j-1)]
+       }
+    }
+  ret
+  }
+
+# As dif() and div() above, but only change of slope is extracted
+# mat - matrix nxm to compute
+# rowOrColumn - 1 makes divisions by rows, while 2 makes divisions by column.
+#               Default is 1
+# returns - a matrix (n-1)xm or nx(m-1) (depending on rowOrColumns) where row/
+#           column i is a vector on {-1,1} where aij is 1 if original row/column
+#           (i-1) is smaller than row/column i, and -1 otherwise
+sig=function(mat,rowOrColumn=1)
+  {
+  n=dim(mat)[1]
+  m=dim(mat)[2]
+
+  if(rowOrColumn==1)
+    {
+    ret=matrix(NA,n,m)
+    ret[1,]=rep(0,m)
+    for(i in 2:n)
+       {
+       for(j in 1:m)
+        {
+        if(mat[i,j]>mat[(i-1),j]) ret[i,j]=1
+        if(mat[i,j]<mat[(i-1),j]) ret[i,j]=-1
+        if(mat[i,j]==mat[(i-1),j]) ret[i,j]=0
+        }
+       }
+    }
+  else if(rowOrColumn==2)
+    {
+    ret=matrix(NA,n,m)
+    ret[,1]=rep(0,n)
+    for(j in 2:m)
+       {
+       for(i in 1:n)
+        {
+        if(mat[i,j]>mat[i,(j-1)]) ret[i,j]=1
+        if(mat[i,j]<mat[i,(j-1)]) ret[i,j]=-1
+        if(mat[i,j]==mat[i,(j-1)]) ret[i,j]=0
+        }
+       }
+    }
+  ret
+  }
+
+
+#Computes variance of input matrix mat by rows or columns as the
+# sum of euclidean distances between all rows/columns, divided by 1/n(n-1),
+# being n the number of rows or columns.
+# Zero variance implies an homogeneous matrix by rows or columns
+# BIC NOTE: This measure is only valid for biclusters based on constants values
+# (by rows, columns, or both) or coherent evolutions based on grouping next values
+# We hope it can be used by biclusters of coherent values (additive or multiplicative)
+# by applying a previous differential treatment that not change variance measure
+# in case of constant values.
+# To do this, first apply differential treatmen with sig(), dif() or div()
+# as follows
+# Variance on genes -> variance(dif(M,2))
+# Variance on conditions -> variance(dif(M),2)
+variance=function(mat,rowOrColumn=1)
+  {
+  n=dim(mat)[1]
+  m=dim(mat)[2]
+  ret=0
+  
+  if(rowOrColumn==1)         #rows
+    {
+    distan=as.matrix(dist(mat))
+    ret=(1/(n*(n-1)))*sum(dist(mat))
+    }
+  else
+    {
+    if(rowOrColumn==2)  #cols
+      {
+      distan=as.matrix(dist(t(mat)))
+      ret=(1/(m*(m-1)))*sum(dist(t(mat)))
+      }
+    else 
+      {
+      }
+      
+    }
+    
+  ret
+  }
+
+#Gives overall variance as mean of row and column variance() above
+overallVariance=function(mat1)
+  {
+  rv=variance(mat1)
+  cv=variance(mat1,2)
+  n=dim(mat)[1]
+  m=dim(mat)[2]
+  
+  ret=(n*rv+m*cv)/(n+m)
+  ret
+  }  
+
+#Returns a dimxdim matrix P where pij=1/(n+1) where n is the number of 
+#biclusters where rows/columns i and j appear grouped together. pij will be in
+#[1/max,1] where 1 is achieved if they are never grouped and 1/max if they are
+#grouped in all the biclusters of the result set.
+
+  
+ 
+# Compute coherence measures for resultSet. resultSet must have a bicluster
+# result syntax, concreetly $rows and $cols vectors for each bicluster, as
+# returned by bimax() or plaid(). mat is the matrix over which resultSet applies
+# Returns row and column variance, additive, multiplicative and sign coherence
+# for each biclusters, and overall quantities for all the result set:
+#   resultSet$rv - row variance
+#            $cv - col variance
+#            $rac - row additive coherence
+#            $cac - column additive coherence
+#            $rmc - row multiplicative coherence
+#            $cmc - column multiplicative coherence
+#            $rsc - row sign coherence
+#            $csc - column sign coherence
+# 
+coherenceAnalysis=function(mat, resultSet)
+  {
+  overallRowVariance=0
+  overallColVariance=0
+  overallRowAddCoherence=0
+  overallColAddCoherence=0
+  overallRowMulCoherence=0
+  overallColMulCoherence=0
+  overallRowSignCoherence=0
+  overallColSignCoherence=0
+  N=0
+  M=0
+  ret=c()
+  
+  for(i in 1:length(resultSet))
+    {
+    rows=resultSet[[i]]$rows
+    cols=resultSet[[i]]$cols
+    A=mat[rows,cols]
+    ret[[i]]=list(rows=rows,cols=cols,
+            rowVariance=variance(A,1), colVariance=variance(A,2), 
+            rowAddCoherence=variance(dif(A,2)), colAddCoherence=variance(dif(A),2), 
+            rowMulCoherence=variance(div(A,2)), colMulCoherence=variance(div(A),2),
+            rowSignCoherence=variance(sig(A,2)), colSignCoherence=variance(sig(A),2) )
+  
+    N=N+length(rows)
+    M=M+length(cols)
+  overallRowVariance=overallRowVariance+length(rows)*ret[[i]]$rowVariance
+  overallColVariance=overallColVariance+length(cols)*ret[[i]]$colVariance
+  overallRowAddCoherence=overallRowAddCoherence+length(rows)*ret[[i]]$rowAddCoherence
+  overallColAddCoherence=overallColAddCoherence+length(cols)*ret[[i]]$colAddCoherence
+  overallRowMulCoherence=overallRowMulCoherence+length(rows)*ret[[i]]$rowMulCoherence
+  overallColMulCoherence=overallColMulCoherence+length(cols)*ret[[i]]$colMulCoherence
+  overallRowSignCoherence=overallRowSignCoherence+length(rows)*ret[[i]]$rowSignCoherence
+  overallColSignCoherence=overallColSignCoherence+length(cols)*ret[[i]]$colSignCoherence
+    }
+    
+  ret$rv=overallRowVariance/N
+  ret$cv=overallColVariance/M
+  ret$v=(ret$rv*N+ret$cv*M)/(N+M)
+  ret$rac=overallRowAddCoherence/N
+  ret$cac=overallColAddCoherence/M
+  ret$ac=(ret$rac*N+ret$cac*M)/(N+M)
+  ret$rmc=overallRowMulCoherence/N
+  ret$cmc=overallColMulCoherence/M
+  ret$mc=(ret$rmc*N+ret$cmc*M)/(N+M)
+  ret$rsc=overallRowSignCoherence/N
+  ret$csc=overallColSignCoherence/M
+  ret$sc=(ret$rsc*N+ret$csc*M)/(N+M)
+  ret
+  }
+  
+#  bimaxCoherence=coherenceAnalysis(A,bicBimax)
+#  plaidCoherence=coherenceAnalysis(A,bicPlaid)
+
