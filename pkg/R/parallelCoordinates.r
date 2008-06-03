12b808915b73a06f3b5100512b90ccf45da4749f
kingsebi (Sebastian Kaiser) <sebastian.kaiser@stat.uni-muenchen.de>
2008-06-03 09:35:32 +0000

    Changed Parralellel Coordinates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biclust@104 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/parallelCoordinates.r
+++ pkg/R/parallelCoordinates.r
@@ -1,4 +1,4 @@
-parallelCoordinates=function(x, bicResult, number, plotConditions=TRUE, absoluteLimits=TRUE)
+parallelCoordinates=function(x, bicResult, number, plotConditions=TRUE, absoluteLimits=TRUE,geneTitle=FALSE,...)
   {
   n=dim(x)[1]
   m=dim(x)[2]
@@ -6,7 +6,8 @@ parallelCoordinates=function(x, bicResult, number, plotConditions=TRUE, absolute
   bicRows=row(matrix(bicResult@RowxNumber[,number]))[bicResult@RowxNumber[,number]==T]
   bicCols=row(matrix(bicResult@NumberxCol[number,]))[bicResult@NumberxCol[number,]==T]
 
-
+  if(geneTitle)
+  {
   if(absoluteLimits)
     {
     if(plotConditions)  matplot(x[bicRows,bicCols],type='l',lty=1, ylab="Expression level", xlab="Gene", ylim=c(min(x),max(x)))
@@ -17,10 +18,28 @@ parallelCoordinates=function(x, bicResult, number, plotConditions=TRUE, absolute
     if(plotConditions)  matplot(x[bicRows,bicCols],type='l',lty=1, ylab="Expression level", xlab="Gene")
     else                matplot(t(x[bicRows,bicCols]),type='l',lty=1, ylab="Expression level", xlab="Condition")
     }
+
   if(plotConditions)
-      title(main=paste("Expresion levels of conditions \nin Bicluster",number," across their genes\n",
+      title(main=paste("Expression levels of conditions \nin Bicluster",number," across their genes\n",
         "(genes=", length(bicRows), "conditions=",length(bicCols),")",sep=" "))
   else
-      title(main=paste("Expresion levels of genes \nin Bicluster",number," across their conditions\n",
+      title(main=paste("Expression levels of genes \nin Bicluster",number," across their conditions\n",
         "(genes=", length(bicRows), "conditions=",length(bicCols),")",sep=" "))
+  
   }
+ 
+else
+{
+
+if(absoluteLimits)
+    {
+    if(plotConditions)  matplot(x[bicRows,bicCols],type='l',lty=1, ylim=c(min(x),max(x)),...)
+    else                matplot(t(x[bicRows,bicCols]),type='l',lty=1, ylim=c(min(x),max(x)),...)
+    }
+  else
+    {
+    if(plotConditions)  matplot(x[bicRows,bicCols],type='l',lty=1,...)
+    else                matplot(t(x[bicRows,bicCols]),type='l',lty=1,...)
+    }
+}
+}
