f40d7afec62bffa037e15a2c0b8534d9967a0b42
kingsebi (Sebastian Kaiser) <sebastian.kaiser@stat.uni-muenchen.de>
2009-10-02 15:30:08 +0000

    Simple Predict Funktion for Bimay
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biclust@149 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/predict.r
+++ pkg/R/predict.r
@@ -1,16 +1,23 @@
 predict.BCBimax<-function(BCBimax,x)
 {
+le<-dim(x)[1]
+count<-BCBimax@Number
+coln<-rowSums(BCBimax@NumberxCol)
+res<-vector("integer",length=le)
+count
+for(i in 1:le)
+  {
+  xtest<-x[i,]
+  for(j in 1:count)
+    {
+    if(sum(xtest[BCBimax@NumberxCol[j,]])==coln[j])
+      {
+      res[i]<-j
+      break
+      }      
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
+#res[is.na(res)]<-0
+res
 }
